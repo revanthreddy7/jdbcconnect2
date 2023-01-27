30dfcf1eb439981ee418dc6b6f7bf2d936f8287c
Abhinav Shinow <55484189+alphabeta2206@users.noreply.github.com>
2023-01-27 14:26:45 +0530

    Added Game Logic Class

---
+++ Game.java
@@ -0,0 +1,72 @@
+import java.util.*;
+public class Game {
+    private int betAmt;
+    private final int betType;
+    private int luckyNumber;
+    Scanner sc = new Scanner(System.in);
+
+    public Game(int betAmt, int betType){
+        this.betAmt = betAmt;
+        this.betType = betType;
+    }
+    public int getRandom(int min, int max){
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+    public int playRoulette(){
+
+        int rand = getRandom(0,36);
+        int rouletteVal = rand%2;
+        //even bet
+        if (betType == 1 && rouletteVal == 0){
+            return betAmt*2;
+        }
+        //odd bet
+        else if (betType == 2 && rouletteVal == 1) {
+            return betAmt*2;
+        }
+        else if (betType == 3){
+            System.out.println("Please Enter the lucky number you wish to bet on");
+            this.luckyNumber = sc.nextInt();
+
+            if (luckyNumber>36 || luckyNumber<0){
+                this.playRoulette();
+            }
+            //Number Bet
+            if(luckyNumber == rand){
+                return betAmt*10;
+            }
+        }
+
+        return 0;
+    }
+    public int playDice(){
+
+        int diceOne = getRandom(0,6);
+        int diceTwo = getRandom(0,6);
+
+        int diceVal = (diceOne + diceTwo)%2;
+
+        //even sum bet
+        if (betType == 1 && diceVal == 0){
+            return betAmt*2;
+        }
+        // odds sum bet
+        else if (betType == 2 && diceVal == 1) {
+            return betAmt*2;
+        }
+        // sum bet
+        else if (betType == 3){
+            System.out.println("Please Enter the dice sum you wish to bet on");
+            this.luckyNumber = sc.nextInt();
+            if (luckyNumber>12 || luckyNumber<2){
+                this.playDice();
+            }
+            if(luckyNumber == diceOne+diceTwo){
+                return betAmt*10;
+            }
+        }
+
+        return 0;
+    }
+}
