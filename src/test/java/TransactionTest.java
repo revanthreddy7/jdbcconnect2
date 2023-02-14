a40870c532b115210e7cfb199f7aea5139fb5b67
pp-in-564 <abhinavshinow@gmail.com>
2023-02-14 14:53:43 +0530

    added parameter resolver to test

---
+++ src/test/java/TransactionTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+@DisplayName("User Test")
+@ExtendWith(UserParameterResolver.class)
+public class TransactionTest {
+
+
+}
